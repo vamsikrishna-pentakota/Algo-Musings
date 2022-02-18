cc702e016eeae6503724f785ca455f1c635734a1
vamsi-pentakota <vamsipentakota@gmail.com>
2022-02-17 23:58:06 -0600

    Tournament Winner problem - Optimal Solution - Unit tests NOT implemented

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Algo-Musings
+
+-- Stylized Code Snippets -- TODO [Learn]
+1. map.put(key, map.getOrDefault(key, defaultValue))
+2. String output = (result == RESULT_FROM_COMPUTATION) ? foo : bar;
---
+++ TournamentWinner.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class TournamentWinner{
+    // tournamentList = [[H,A],[H,A],[H,A]]
+    //                  [[HTML,C#],[Python,HTML],[Python,HTML]] = [1 , 0, 0]
+
+    public static String decideWinner(String[][] tournamentList , int[] matchResults){
+        HashMap<String, Integer> scoreboard = new HashMap<>();
+        String bestTeam = "";
+        for(int i=0; i<tournamentList.length; i++){
+            String winningTeam = (matchResults[i] == 0) ? tournamentList[i][1] : tournamentList[i][0];
+            if("".equals(bestTeam)) bestTeam = winningTeam;
+            scoreboard.put(winningTeam, scoreboard.getOrDefault(winningTeam, 0) + 3);
+            if(scoreboard.get(winningTeam) > scoreboard.get(bestTeam)) bestTeam = winningTeam;
+        }        
+        return bestTeam;  
+    }
+    public static void main(String[] args) {
+        String [][] competitions = {{"HTML","C#"},{"Python","HTML"},{"Python","HTML"}};
+        int [] results = {1,1,0}; 
+        System.out.println(decideWinner(competitions, results)); 
+    }
+}
